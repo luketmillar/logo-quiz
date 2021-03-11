1490f7684bd87af60ba7d642ba895bac2835bd43
Luke Millar <luketmillar@gmail.com>
2021-03-11 01:43:41 -0800

    Smaller logo images

--- src/components/Quiz/Questions.ts
+++ src/components/Quiz/Questions.ts
@@ -1,21 +1,21 @@
 import { Option } from "./Question/types"
 
 const questions = [
+    { name: 'Spotify', answer: Option.A },
     { name: 'Facebook', answer: Option.A },
     { name: 'Adobe', answer: Option.B },
     { name: 'Microsoft', answer: Option.B },
     { name: 'Amazon', answer: Option.A },
     { name: 'Apple', answer: Option.A },
+    { name: 'Netflix', answer: Option.B },
     { name: 'Reddit', answer: Option.B },
     { name: 'TikTok', answer: Option.B },
+    { name: 'Airbnb', answer: Option.A },
     { name: 'Twitter', answer: Option.B },
     { name: 'Robinhood', answer: Option.A },
-    { name: 'Google', answer: Option.B },
     { name: 'Zillow', answer: Option.A },
-    { name: 'Spotify', answer: Option.A },
     { name: 'iPhone camera', answer: Option.B },
-    { name: 'Netflix', answer: Option.B },
-    { name: 'Airbnb', answer: Option.A },
+    { name: 'Google', answer: Option.B },
 ]
 
 export default questions
