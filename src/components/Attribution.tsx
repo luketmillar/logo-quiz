4139a48ca892decaf68f923af244eb28d26b0fa6
Luke Millar <luketmillar@gmail.com>
2021-03-19 17:51:05 -0700

    Attribution on iphone

--- src/components/Attribution.tsx
+++ src/components/Attribution.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { ButtonCss } from './Button'
 
-const Att = styled.a`
+const Att = styled.a<{ position: 'top' | 'bottom' }>`
     ${ButtonCss}
     text-decoration: none;
     display: flex;
@@ -13,12 +13,12 @@ const Att = styled.a`
         background-color: #BC00B4;
     }
     @media screen and (max-width: 500px), screen and (max-height: 500px) {
-        display: none;
+        ${props => props.position === 'top' && 'display: none;'}
     }
 `
 
 const Attribution = ({ position = 'bottom' }: { position?: 'top' | 'bottom' }) => {
-    return <Att style={{ position: 'fixed', ...(position === 'bottom' ? { bottom: 0 } : { top: 0 }), left: 0 }} href="https://twitter.com/ltm" target="_blank"> by @ltm</Att >
+    return <Att position={position} style={{ position: 'fixed', ...(position === 'bottom' ? { bottom: 0 } : { top: 0 }), left: 0 }} href="https://twitter.com/ltm" target="_blank"> by @ltm</Att >
 }
 
 export default Attribution
