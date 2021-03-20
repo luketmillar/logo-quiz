04c2b96eb7be831083ade2f33bf0835cba1a409b
Luke Millar <luketmillar@gmail.com>
2021-03-19 17:54:33 -0700

    Netlify tag on form

--- src/components/Quiz/Subscribe.tsx
+++ src/components/Quiz/Subscribe.tsx
@@ -59,7 +59,7 @@ const Subscribe = () => {
             <>
                 <Message>Wanna hear about future quizes, games, and projects? Subscribe!</Message>
                 <div style={{ height: 20 }} />
-                <Form onSubmit={handleSubmit}>
+                <Form onSubmit={handleSubmit} method="POST" data-netlify="true">
                     <input ref={emailRef} style={{ height: 58, width: 300, boxSizing: 'border-box', borderRadius: 0, border: '1px solid black', fontSize: 18, padding: '12px 18px' }} type="email" name="email" placeholder="email@address.com" />
                     <Submit type="submit">Subscribe</Submit>
                 </Form>
