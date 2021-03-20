3d7736b07013be5fd769e2be83444a1266d0e34d
Luke Millar <luketmillar@gmail.com>
2021-03-19 18:06:47 -0700

    Hmmm

--- src/components/Quiz/Subscribe.tsx
+++ src/components/Quiz/Subscribe.tsx
@@ -51,6 +51,7 @@ const Subscribe = () => {
                 "email": emailRef.current?.value
             })
         }).then(() => setSubscribed(true), () => setSubscribed(true))
+        return false
     }
     return <Container>
         {subscribed ? <Message>Thank you for subscribing!</Message> :
